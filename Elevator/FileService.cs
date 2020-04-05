675316f715a9ed86f34c0008b742a0c91cfa705c
Vildmusen <viktorvilmusenaho@hotmail.com>
2020-04-05 17:40:42 +0200

    added fileservice to read and convert input from sample.txt

---
+++ Elevator/FileService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Elevator
+{
+    public class FileService
+    {
+        static string path = Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory()));
+        static string file = "/../sample.txt";
+
+        public static List<int[]> GetPassengers()
+        {
+            List<int[]> result = new List<int[]>();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(path + file);
+                result = ConvertToIntList(lines);
+            } catch (Exception e)
+            {
+                Console.WriteLine("Error reading input file: " + e.Message);
+            }
+
+            return result;
+        }
+
+        private static List<int[]> ConvertToIntList(string[] lines)
+        {
+            List<int[]> res = new List<int[]>();
+            foreach(string line in lines)
+            {
+                string[] separated = line.Split(",");
+                int[] numbers = new int[separated.Length];
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    try
+                    {
+                        numbers[i] = Int32.Parse(separated[i]);
+                    } catch (Exception e)
+                    {
+                        Console.WriteLine("Error read values in input file: " + e.Message);
+                    }
+                }
+                res.Add(numbers);
+            }
+            return res;
+        }
+    }
+}
--- Elevator/Program.cs
+++ Elevator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Elevator
 {
@@ -6,9 +7,30 @@ namespace Elevator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!\n");
+
+            List<int[]> passengers = FileService.GetPassengers();
+
+            Console.WriteLine("file contains: ");
+
+            foreach (int[] line in passengers)
+            {
+                printline(line);
+            }
+
+            Console.WriteLine();
             Console.WriteLine("Press ANYTHING to exit...");
             Console.ReadLine();
+        }
+
+        static void printline(int[] line)
+        {
+            for (int i = 0; i < line.Length - 1; i++)
+            {
+                Console.Write(line[i] + ", ");
+            }
+            Console.Write(line[line.Length-1]);
+            Console.WriteLine();
         }
     }
 }
